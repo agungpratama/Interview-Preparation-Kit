57e55007d1c68aed772479681b98a9bc37a6ab70
Agung Pratama <agung1991pratama@yahoo.co.id>
2022-04-22 08:36:42 +0700

    solves problem alternating characters

---
+++ String Manipulation/Alternating Characters/alternating-characters.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int alternatingCharacters(string s) {
+    char currChar = ' '; 
+    int deletionCount = 0;
+     for (int i = 0; i < s.size(); i++){
+        cout << s[i];      
+        if (currChar != s[i]) {
+            currChar = s[i];
+            
+        }else {
+            deletionCount++;
+        }
+    }
+
+    cout << "Deletion count: " << deletionCount << endl;
+    return deletionCount;
+
+}
+
+int main() {
+    alternatingCharacters("AAABBB");
+    return 1;
+}
